8993ccfdd4aa342bb74dee55884c4aef0622390d
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-09-29 19:56:03 +0300

    units 4-5

--- script.js
+++ script.js
@@ -28,7 +28,16 @@
 
 /////////////////////// unit 4 //////////////////////////////
 
+// let receivables = 10;
+// let payables = 8;
+// let netWorkingCapital = receivables - payables;
+// console.log(`Net working capital is: ${netWorkingCapital}`);
+
 /////////////////////// unit 5 //////////////////////////////
+
+// let myArray = [1, 2, 3];
+// let arraySum = myArray.reduce((previous, current) =>  previous + current);
+// console.log(`Sum of array values is: ${arraySum}`);
 
 /////////////////////// unit 6 //////////////////////////////
 
