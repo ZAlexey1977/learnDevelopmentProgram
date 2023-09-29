75603d15b2265ce63557545cd2f5d3185e03f29e
ZAlexey1977 <zayatsalexey1@gmail.com>
2023-09-29 20:02:14 +0300

    units 8-9

--- script.js
+++ script.js
@@ -60,7 +60,25 @@
 
 /////////////////////// unit 8 //////////////////////////////
 
+// function getNine() {
+//   let x = 6;
+//   let y = 3;
+//   return x + y;
+// }
+
+// let result = getNine();
+// console.log(result);
+
 /////////////////////// unit 9 //////////////////////////////
+
+// function raiseToPower(b, e) {
+//   return Math.pow(b, e);
+// }
+
+// let base = 2;
+// let exp = 3;
+// let power = raiseToPower(base, exp);
+// console.log(power);
 
 /////////////////////// unit 10 //////////////////////////////
 
